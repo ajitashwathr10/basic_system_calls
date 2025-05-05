f2cc77d7310a22efce3d2a90b388b4ce3fffc2a6
Ajit Ashwath <141044380+ajitashwathr10@users.noreply.github.com>
2025-05-05 09:11:28 +0530

    Update dining_philosopher.c

--- expected_ques/dining_philosopher.c
+++ expected_ques/dining_philosopher.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
+#define N 5  // Number of philosophers
+
+pthread_mutex_t forks[N];
+
+void* philosopher(void* arg) {
+    int id = *(int*)arg;
+    int left = id;
+    int right = (id + 1) % N;
+
+    while (1) {
+        printf("Philosopher %d is thinking.\n", id);
+        sleep(1);
+
+        pthread_mutex_lock(&forks[left]);
+        pthread_mutex_lock(&forks[right]);
+
+        printf("Philosopher %d is eating.\n", id);
+        sleep(2);
+
+        pthread_mutex_unlock(&forks[right]);
+        pthread_mutex_unlock(&forks[left]);
+
+        printf("Philosopher %d finished eating.\n", id);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t phils[N];
+    int ids[N];
+
+    for (int i = 0; i < N; i++)
+        pthread_mutex_init(&forks[i], NULL);
+
+    for (int i = 0; i < N; i++) {
+        ids[i] = i;
+        pthread_create(&phils[i], NULL, philosopher, &ids[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+        pthread_join(phils[i], NULL);
+
+    return 0;
+}
